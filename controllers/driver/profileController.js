f05e8050bf2079edf077359dd1164170bebc3282
Nikhil <nik.theappideas@gmail.com>
2023-04-13 15:23:06 +0530

    driver select country city

--- controllers/driver/profileController.js
+++ controllers/driver/profileController.js
@@ -1,6 +1,8 @@
 const multilingualUser = require('../../utils/multilingualUser');
 
 const Driver = require('../../models/driverModel');
+const Country = require('../../models/countryModel');
+const City = require('../../models/cityModel');
 
 exports.getProfile = async (req, res, next) => {
     try {
@@ -31,6 +33,9 @@ exports.editProfile = async (req, res, next) => {
 
         driver = multilingualUser(driver, req);
 
+        // Hide fields
+        driver.location = undefined;
+
         res.json({ code: '1', message: req.t('success'), driver });
     } catch (error) {
         next(error);
@@ -43,6 +48,49 @@ exports.deleteProfile = async (req, res, next) => {
         await Driver.findByIdAndDelete(req.driver.id);
 
         res.json({ code: '1', message: req.t('deleted') });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.getSelectCountryCity = async (req, res, next) => {
+    try {
+        const driver = multilingualUser(req.driver, req);
+
+        res.json({
+            code: '1',
+            message: req.t('success'),
+            data: { city: driver.city, country: driver.country },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.postSelectCountryCity = async (req, res, next) => {
+    try {
+        // get city and country by id
+        const { city_id, country_id } = req.body;
+        const [city, country] = await Promise.all([
+            City.findOne({ city_id }),
+            Country.findOne({ country_id }),
+        ]);
+        if (!city) return next(createError.BadRequest('Invalid city_id'));
+        if (!country) return next(createError.BadRequest('Invalid country_id'));
+
+        // update driver
+        let driver = req.driver;
+        driver.city = city.id;
+        driver.country = country.id;
+        await driver.save();
+        await driver.populate('city country');
+        driver = multilingualUser(driver, req);
+
+        res.json({
+            code: '1',
+            message: req.t('success'),
+            data: { city: driver.city, country: driver.country },
+        });
     } catch (error) {
         next(error);
     }
--- routes/driver/profileRoutes.js
+++ routes/driver/profileRoutes.js
@@ -6,14 +6,18 @@ const profileController = require('../../controllers/driver/profileController');
 
 // profile
 router.get('/get_profile_data', checkDriver, profileController.getProfile);
-
 router.post(
     '/edit_profile',
     fileUpload(),
     checkDriver,
     profileController.editProfile
 );
-
 router.get('/delete_profile', checkDriver, profileController.deleteProfile);
+
+// country_city
+router
+    .route('/select_country_city')
+    .get(checkDriver, profileController.getSelectCountryCity)
+    .post(fileUpload(), checkDriver, profileController.postSelectCountryCity);
 
 module.exports = router;
