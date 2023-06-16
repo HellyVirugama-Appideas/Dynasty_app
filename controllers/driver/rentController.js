90669fa589c2907db043d7176a6cbcff7ba947f9
Nikhil <nik.theappideas@gmail.com>
2023-06-16 17:22:29 +0530

    accept request driver

--- controllers/driver/rentController.js
+++ controllers/driver/rentController.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 
 const BookingReq = require('../../models/bookingReqModel');
+const Notification = require('../../models/notificationModel');
 
 exports.getRequests = async (req, res, next) => {
     try {
@@ -15,6 +16,34 @@ exports.getRequests = async (req, res, next) => {
             bookings,
         });
     } catch (error) {
+        next(error);
+    }
+};
+
+exports.acceptRequest = async (req, res, next) => {
+    try {
+        const request = await BookingReq.findByIdAndUpdate(
+            req.params.id,
+            { status: 'accepted' },
+            { new: true }
+        );
+
+        if (!request)
+            return next(createError.BadRequest('Invalid request id.'));
+
+        // Notify user
+
+        Notification.create({
+            user: request.user,
+            message: 'Booking request accepted.',
+        }).catch(error => {
+            console.log('Error creating notification: ', error);
+        });
+
+        res.json({ code: '1', message: req.t('success') });
+    } catch (error) {
+        if (error.name === 'CastError')
+            return next(createError.BadRequest('Invalid request id.'));
         next(error);
     }
 };
--- routes/driver/rentRoutes.js
+++ routes/driver/rentRoutes.js
@@ -5,6 +5,8 @@ const rentController = require('../../controllers/driver/rentController');
 
 router.get('/requests', checkDriver, rentController.getRequests);
 
+router.get('/requests/accept/:id', checkDriver, rentController.acceptRequest);
+
 router.get('/requests/reject/:id', checkDriver, rentController.rejectRequest);
 
 module.exports = router;
