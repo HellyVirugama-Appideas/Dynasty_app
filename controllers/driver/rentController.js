b0dba8feb4c03bff5a3589ed193b776b1dcca9d0
Nikhil <nik.theappideas@gmail.com>
2023-06-16 15:33:03 +0530

    driver rejectRequest api

--- controllers/driver/rentController.js
+++ controllers/driver/rentController.js
@@ -1,3 +1,5 @@
+const createError = require('http-errors');
+
 const BookingReq = require('../../models/bookingReqModel');
 
 exports.getRequests = async (req, res, next) => {
@@ -13,6 +15,21 @@ exports.getRequests = async (req, res, next) => {
             bookings,
         });
     } catch (error) {
+        next(error);
+    }
+};
+
+exports.rejectRequest = async (req, res, next) => {
+    try {
+        const request = await BookingReq.findByIdAndDelete(req.params.id);
+
+        if (!request)
+            return next(createError.BadRequest('Invalid request id.'));
+
+        res.json({ code: '1', message: req.t('success') });
+    } catch (error) {
+        if (error.name === 'CastError')
+            return next(createError.BadRequest('Invalid request id.'));
         next(error);
     }
 };
--- routes/driver/rentRoutes.js
+++ routes/driver/rentRoutes.js
@@ -5,4 +5,6 @@ const rentController = require('../../controllers/driver/rentController');
 
 router.get('/requests', checkDriver, rentController.getRequests);
 
+router.get('/requests/reject/:id', checkDriver, rentController.rejectRequest);
+
 module.exports = router;
