92465719c488d3f707d185ae93a3721f04f94c1c
Nikhil <nik.theappideas@gmail.com>
2023-08-14 16:15:43 +0530

    admin add fixed car delivering fee

--- models/chargesModel.js
+++ models/chargesModel.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose');
 
 const chargesSchema = new mongoose.Schema({
-    baseFare: {
-        type: Number,
-        required: true,
-    },
-    minimumFare: {
-        type: Number,
-        required: true,
-    },
-    bookingFee: {
-        type: Number,
-        required: true,
-    },
+    baseFare: { type: Number, required: true },
+    minimumFare: { type: Number, required: true },
+    bookingFee: { type: Number, required: true },
+    carDeliveringFee: { type: Number, required: true },
 });
 
 module.exports = new mongoose.model('Charge', chargesSchema);
