ffc48ada0f119827edf7c52e10a7c091a39ff046
Nikhil <nik.theappideas@gmail.com>
2023-12-04 18:44:59 +0530

    book and schedule in same api

--- controllers/user/rideController.js
+++ controllers/user/rideController.js
@@ -87,140 +87,8 @@ exports.getVehicleTypes = async (req, res, next) => {
 
 exports.bookRide = async (req, res, next) => {
     try {
-        const nearbyDrivers = await Driver.find({
-            location: {
-                $near: {
-                    $geometry: {
-                        type: 'Point',
-                        coordinates: [req.body.pickupLng, req.body.pickupLat],
-                    },
-                    $maxDistance: process.env.radiusInMeters,
-                },
-            },
-            type: req.body.type,
-            status: 'online',
-        }).limit(5); // Limit to 5 closest drivers
+        const isScheduled = req.body.scheduleTime !== undefined;
 
-        if (nearbyDrivers.length === 0)
-            return next(createError.BadRequest('ride.fail'));
-
-        const ride = await RideReq.create({
-            user: req.user.id,
-            pickupAddress: req.body.pickupAddress,
-            pickupLat: req.body.pickupLat,
-            pickupLng: req.body.pickupLng,
-            endAddress: req.body.endAddress,
-            endLat: req.body.endLat,
-            endLng: req.body.endLng,
-            type: req.body.type,
-        });
-        await ride.populate('user', 'name phone');
-
-        // Calculate distance for each driver
-        const drivers = nearbyDrivers.map(driver => {
-            const driverLocation = {
-                latitude: driver.location.coordinates[1],
-                longitude: driver.location.coordinates[0],
-            };
-            const pickupLocation = {
-                latitude: ride.pickupLat,
-                longitude: ride.pickupLng,
-            };
-
-            // Calculate distance
-            const distanceInMeters = geolib.getDistance(
-                driverLocation,
-                pickupLocation
-            );
-            const distanceInKm = (distanceInMeters / 1000).toFixed(1);
-
-            const timeInMinutes = (distanceInKm / 30) * 60; // Est. speed 30 km/h
-            const time =
-                timeInMinutes < 1
-                    ? 'less than a minute away'
-                    : `${Math.round(timeInMinutes)} minutes away`;
-
-            const distance =
-                distanceInMeters < 100
-                    ? `${distanceInMeters} meter away`
-                    : `${distanceInKm} km away`;
-
-            return { id: driver.id, distance, time };
-        });
-
-        // Notify drivers
-        const { driverId, time, distance } = await notifyDrivers(drivers, ride);
-
-        // If no one accepted
-        if (driverId === null)
-            return res.json({ code: '0', message: req.t('ride.fail') });
-
-        // Create ride
-        let rideResponse = await Ride.create({
-            ...ride._doc,
-            driver: driverId,
-            time,
-            distance,
-            otp: generateCode(6),
-            status: 'Ongoing',
-        });
-
-        await Driver.findByIdAndUpdate(driverId, { status: 'busy' });
-
-        // Populate driver with type
-        await rideResponse.populate({
-            path: 'driver',
-            populate: {
-                path: 'type',
-                select: '-__v -distanceRate -typeFor -capacity',
-            },
-            select: 'name profile phone',
-        });
-
-        rideResponse = rideResponse._doc;
-        rideResponse.type = multilingual(rideResponse.driver.type, req);
-        rideResponse.driver.type = undefined;
-        rideResponse.__v = undefined;
-
-        res.json({
-            code: '1',
-            message: req.t('ride.success'),
-            ride: rideResponse,
-        });
-    } catch (error) {
-        if (error.name == 'CastError')
-            return next(createError.BadRequest('Invalid type id.'));
-        next(error);
-    }
-};
-
-exports.cancelRide = async (req, res, next) => {
-    try {
-        const ride = await Ride.findById(req.body.rideId);
-
-        if (!ride || ['Completed', 'Cancelled'].includes(ride.status))
-            return next(createError.NotFound('Ride not found with given id.'));
-
-        ride.status = 'Cancelled';
-        ride.cancellationReason = req.body.cancellationReason;
-
-        // Save ride and Update driver status to online
-        await Promise.all([
-            ride.save(),
-            Driver.findByIdAndUpdate(ride.driver, { status: 'online' }),
-        ]);
-
-        // Notify driver
-        io.to(ride.driver.toString()).emit('cancelRide', { ride });
-
-        res.json({ code: '1', message: req.t('ride.cancel') });
-    } catch (error) {
-        next(error);
-    }
-};
-
-exports.scheduleRide = async (req, res, next) => {
-    try {
         const nearbyDrivers = await Driver.find({
             location: {
                 $near: {
@@ -297,8 +165,11 @@ exports.scheduleRide = async (req, res, next) => {
             time,
             distance,
             otp: generateCode(6),
-            status: 'Upcoming',
+            status: isScheduled ? 'Upcoming' : 'Ongoing',
         });
+
+        if (!isScheduled)
+            await Driver.findByIdAndUpdate(driverId, { status: 'busy' });
 
         // Populate driver with type
         await rideResponse.populate({
@@ -317,12 +188,37 @@ exports.scheduleRide = async (req, res, next) => {
 
         res.json({
             code: '1',
-            message: req.t('ride.schedule'),
+            message: req.t(isScheduled ? 'ride.schedule' : 'ride.success'),
             ride: rideResponse,
         });
     } catch (error) {
         if (error.name == 'CastError')
             return next(createError.BadRequest('Invalid type id.'));
+        next(error);
+    }
+};
+
+exports.cancelRide = async (req, res, next) => {
+    try {
+        const ride = await Ride.findById(req.body.rideId);
+
+        if (!ride || ['Completed', 'Cancelled'].includes(ride.status))
+            return next(createError.NotFound('Ride not found with given id.'));
+
+        ride.status = 'Cancelled';
+        ride.cancellationReason = req.body.cancellationReason;
+
+        // Save ride and Update driver status to online
+        await Promise.all([
+            ride.save(),
+            Driver.findByIdAndUpdate(ride.driver, { status: 'online' }),
+        ]);
+
+        // Notify driver
+        io.to(ride.driver.toString()).emit('cancelRide', { ride });
+
+        res.json({ code: '1', message: req.t('ride.cancel') });
+    } catch (error) {
         next(error);
     }
 };
